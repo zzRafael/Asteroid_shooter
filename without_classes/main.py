2bbd0441be378dbd0a29004e50b33d203b315005
Rafael Alves <citygotham552@gmail.com>
2023-05-31 21:05:21 -0300

    1.0 Version

--- without_classes/main.py
+++ without_classes/main.py
@@ -7,13 +7,14 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption('Asteroid Shooter')
 clock = pygame.time.Clock()
 
+# defs
 def laser_update(laser_list, speed = 1000):
     for rect in laser_list:
         rect.y -= round(speed * dt)
         if rect.bottom < 0:
             laser_list.remove(rect) 
 
-def meteor_update(meteor_list, speed = 300):
+def meteor_update(meteor_list, speed = 250):
     for meteor_tuple in meteor_list:
         direction = meteor_tuple[1]
         meteor_rect = meteor_tuple[0]
@@ -44,23 +45,27 @@ ship_rect = ship_surf.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2) )
 laser_surf = pygame.image.load('without_classes/graphics/laser.png').convert_alpha()
 laser_list = []
 
-# meteors
 meteor_surf = pygame.image.load('without_classes/graphics/meteor.png').convert_alpha()
 meteor_list = []
-
-#laser timer
-can_shoot = True
-shoot_time = None
                                
 # import text
 font = pygame.font.Font('without_classes/graphics/subatomic.ttf', 50)
+
+# laser timer
+can_shoot = True
+shoot_time = None
 
 # meteor timer
 meteor_timer = pygame.event.custom_type()
 pygame.time.set_timer(meteor_timer, 250)
 
+#importing sounds
+laser_sound = pygame.mixer.Sound('without_classes/sounds/laser.ogg')
+explosion_sound = pygame.mixer.Sound('without_classes/sounds/explosion.wav')
+background_music = pygame.mixer.Sound('without_classes/sounds/music.wav')
+background_music.play(loops = -1)
+
 while True:
-    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -73,6 +78,10 @@ while True:
             # timer
             can_shoot = False
             shoot_time = pygame.time.get_ticks()
+
+            #play sound
+            laser_sound.play()
+
         if event.type == meteor_timer:
             # random position
             y_pos = randint(-100,-50)
@@ -95,31 +104,29 @@ while True:
     laser_update(laser_list=laser_list)
     meteor_update(meteor_list=meteor_list)
 
-    #collisions
+    #ship and meteor collision
     for meteor_tuple in meteor_list:
         meteor_rect = meteor_tuple[0]
         if ship_rect.colliderect(meteor_rect):
             print(f'Score: {pygame.time.get_ticks() // 1000}')
             pygame.quit()
             sys.exit()
-    
+    # meteors and lasers collisions
     for laser_rect in laser_list:
         for meteor_tuple in meteor_list:
-            meteor_rect = meteor_tuple[0]
-            if laser_rect.colliderect(meteor_rect):
+            if laser_rect.colliderect(meteor_tuple[0]):
                 meteor_list.remove(meteor_tuple)
                 try:
                     laser_list.remove(laser_rect)
                 except ValueError:
                     pass
+                explosion_sound.play()
+
     # drawing
     display_surface.fill((0,0,0))
-    display_surface.blit(background_surf, (0, 0))
-
-    # display score
-    display_score()
-    # display ship
-    display_surface.blit(ship_surf, ship_rect)
+    display_surface.blit(background_surf, (0, 0)) # display background
+    display_score() # display score
+    display_surface.blit(ship_surf, ship_rect) # display ship
 
     # display laser
     can_shoot = laser_timer(can_shoot)
